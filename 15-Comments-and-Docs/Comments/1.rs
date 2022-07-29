89c647ebce6e4f338a5c05e7bc22b071bfb1648d
Shivam Agarwal <72163800+ShivamAgarwal-code@users.noreply.github.com>
2022-07-30 02:59:28 +0530

    Create 1.rs

---
+++ 15-Comments-and-Docs/Comments/1.rs
@@ -0,0 +1,21 @@
+// Regular comments whihch are ignored by the compiler:
+// Line comment, which goes to the end of the line
+/* Block comment, which goes to the end of the closing delimiter */
+
+// Given
+
+/* Make it work, only using comments! */
+// fn main() {
+//     todo!();
+//     unimplemented!();
+
+//     assert_eq!(6, 5 + 3 + 2 + 1 )
+// }
+
+// My Solution
+fn main() {
+    /*todo!();
+    unimplemented!();
+
+    assert_eq!(6, 5 + 3 + 2 + 1 )*/
+}
